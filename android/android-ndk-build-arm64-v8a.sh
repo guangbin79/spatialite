e96e236b58fecffdfd8901c27a55d7c1c86d4184
guangbin <guangbin79@gmail.com>
2022-01-27 11:04:57 +0800

    init

---
+++ android/android-ndk-build-arm64-v8a.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+TARGET=aarch64-linux-android
+ARCH=android-arm64
+API=21
+TOOLCHAIN=$ANDROID_NDK_HOME/toolchains/llvm/prebuilt/linux-x86_64
+PREFIX=$PWD/build/arm64-v8a
+SYSROOT=$PWD/build/arm64-v8a
+
+export AR=$TOOLCHAIN/bin/llvm-ar
+export CC=$TOOLCHAIN/bin/$TARGET$API-clang
+export AS=$CC
+export CXX=$TOOLCHAIN/bin/$TARGET$API-clang++
+export LD=$TOOLCHAIN/bin/ld
+export RANLIB=$TOOLCHAIN/bin/llvm-ranlib
+export STRIP=$TOOLCHAIN/bin/llvm-strip
+export CFLAGS+="-fPIC"
+export CPPFLAGS+="-fPIC"
+
+echo "xxxbuild zlib"
+cd ../zlib-1.2.11
+./configure --static --prefix=$PREFIX
+make
+make install
+
+echo "xxxbuild iconv"
+cd ../libiconv-1.16
+./configure --host=$TARGET --with-sysroot=$SYSROOT --prefix=$PREFIX --enable-static=yes --enable-shared=no
+make
+make install
+
+echo "xxxbuild sqlite"
+cd ../sqlite-autoconf-3370200
+autoreconf -f -i
+CFLAGS="$CFLAGS -DSQLITE_CORE" ./configure --host=$TARGET --with-sysroot=$SYSROOT  --prefix=$PREFIX --enable-static=yes --enable-shared=no
+make
+make install
+
+echo "xxxbuild proj"
+cd ../proj-4.9.3
+autoreconf -f -i
+./configure --host=$TARGET --with-sysroot=$SYSROOT --prefix=$PREFIX --enable-static=yes --enable-shared=no
+make
+make install
+
+echo "xxxbuild geos"
+cd ../geos-3.9.2
+./configure --host=$TARGET --with-sysroot=$SYSROOT --prefix=$PREFIX --enable-static=yes --enable-shared=no
+make -j6
+make install
+
+echo "xxxbuild spatialite"
+cd ../libspatialite-4.3.0
+CFLAGS="$CFLAGS -ULOADABLE_EXTENSION" PKG_CONFIG_PATH="$SYSROOT/lib/pkgconfig" CPPFLAGS="$CPPFLAGS -I$SYSROOT/include" LDFLAGS="$LDFLAGS -L$SYSROOT/lib -lsqlite3 -lproj -lgeos_c -lgeos" ./configure --host=arm-linux-eabi --prefix=$PREFIX --enable-static=yes --enable-shared=no --enable-freexl=no --enable-libxml2=no  --enable-gcp=yes --enable-examples=no --with-geosconfig=$SYSROOT/bin/geos-config
+make -j6
+make install
